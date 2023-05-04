ca4b65db23a8fe9e70070131fd222320c84980ba
hatim-b <hbbaqaja@gmail.com>
2023-05-04 12:42:31 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b);
 {
-	int a;
-	unsigned int decim_val = 0;
+        int a;
+        unsigned int decim_val = 0;
 
-	if (!b)
-		return (0);
+        if (!b)
+                return (0);
 
-	for (a = 0; b[a]; a++)
-	{
-		if (b[a] < '0' || b[a] > '1')
-			return (0);
-		decim_val = 2 * decim_val + (b[a] - '0');
-	}
+        for (a = 0; b[a]; a++)
+        {
+                if (b[a] < '0' || b[a] > '1')
+                        return (0);
+                decim_val = 2 * decim_val + (b[a] - '0');
+        }
 
-	return (decim_val);
+        return (decim_val);
 }
